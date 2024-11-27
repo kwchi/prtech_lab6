fd93073933b178d720353644bc95dbd6a85dcf29
kwchi <144099685+kwchi@users.noreply.github.com>
2024-11-27 23:03:47 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,7 @@ resource "aws_security_group" "web_app2" {
 resource "aws_instance" "webapp_instance" {
   ami           = "ami-0669b163befffbdfc"
   instance_type = "t2.micro"
-  security_groups= [web_app2]
+  security_groups= [web_app2.name]
   tags = {
     Name = "webapp_instance"
   }
